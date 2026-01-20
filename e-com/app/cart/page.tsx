c2984c60549e4b2beec7df921cd0cd9a99c6fceb
Habibur Rahman Tanim <tanim2612@gmail.com>
2026-01-21 00:08:28 +0600

    cart

--- e-com/app/cart/page.tsx
+++ e-com/app/cart/page.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const page = () => {
   return (
-    <div>
-      cart
+    <div className="flex gap-4">
+      <div>
+        <div className="w-1/2">
+          {/* picture */}
+          <div className="">
+            <Image
+              src="/slider1.avif"
+              alt="Slider 1"
+              width={500}
+              height={500}
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </div>
+        <div>cart</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
